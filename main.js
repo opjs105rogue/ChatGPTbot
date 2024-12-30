62afa42f31bf084f461d6f3616f007b4434e67e5
Toxa-K <originkewai666.com@mail.ru>
2024-12-30 11:58:59 +0300

    add variables for work bot and small fixes

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 "use strict";
-const Telegraf = require('telegraf');
+const { Telegraf } = require("telegraf");
 const message = require("telegraf/filters");
 const dotenv = require("dotenv");
+const { OpenAI } = require("openai");
 dotenv.config();
-const key = process.env.BOT_API_KEY;
-
+const BOT_KEY = process.env.BOT_API_KEY;
+const APENAI_KEY = process.env.GPT_API_KEY;
+const client = new OpenAI({
+    apiKey: APENAI_KEY,
+});
+// console.log(BOT_KEY)
+// console.log(APENAI_KEY)
+const bot = new Telegraf(BOT_KEY);
+bot.on("text", async (ctx) => {
+    console.info("Бот считывает сообщ");
+    const userMessage = ctx.message.text;
+});
+bot.launch();
