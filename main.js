ef9347040427fd5151944f90c2dc23409c972b6e
Toxa-K <originkewai666.com@mail.ru>
2024-12-30 16:33:41 +0300

    Try add GPT API #1

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const { Telegraf } = require("telegraf");
 const message = require("telegraf/filters");
 const dotenv = require("dotenv");
 const { OpenAI } = require("openai");
+const http = require("http");
+const https_proxy_agent_1 = require("https-proxy-agent");
 dotenv.config();
 const BOT_KEY = process.env.BOT_API_KEY;
 const APENAI_KEY = process.env.GPT_API_KEY;
+const agent = new https_proxy_agent_1.HttpsProxyAgent('http://168.63.76.32:3128');
 const client = new OpenAI({
     apiKey: APENAI_KEY,
+    // httpAgent: agent,
 });
 // console.log(BOT_KEY)
 // console.log(APENAI_KEY)
@@ -15,5 +20,23 @@ const bot = new Telegraf(BOT_KEY);
 bot.on("text", async (ctx) => {
     console.info("Бот считывает сообщ");
     const userMessage = ctx.message.text;
+    try {
+        console.info("Запрос отправился...");
+        const response = await client.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: userMessage,
+                },
+            ]
+        });
+        const gptMessage = response.choices[0].message.content;
+        await ctx.reply(gptMessage);
+    }
+    catch (error) {
+        console.error("Error while processing message:", error);
+        await ctx.reply("Произошла ошибка при обработке вашего запроса.");
+    }
 });
 bot.launch();
